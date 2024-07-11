933dd74f47108fd8675bdd2ac7c885a07b5887a0
lensi-ghoghari <lensighoghari1106@gmail.com>
2024-07-11 16:27:37 +0530

    11/07/2024

---
+++ ch-1/2-1/pr.3.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    clrscr();
+    printf("*\t \n");
+    printf("**\t \n");
+    printf("***\t \n");
+    printf("**\t \n");
+    printf("*\t \n");
+
+    getch();
+
+}
---
+++ ch-1/2-1/pr.5.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    clrscr();
+    printf(" * * *    *     *  *      *");
+    printf("*     *   *     *  *      *");
+    printf("*     *   **    *  *      *");
+    printf("* * *     * *   *  *      *");
+    printf("*    *    *   * *  *  *   *");
+    printf("*     *   *     *  * *  * *");
+    printf("*       * *     *  *      *");
+    getch();
+}
